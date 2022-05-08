c0b869652b18d787b8b33dbbf0e5a75ce8f1ee90
CaptainMehdi <mehdi.diyani@gmail.com>
2022-05-08 17:29:35 -0400

    je repris mon tp3 pour faire la partie Spring de mon Tp4. Je m'apprete a changer le controller et ajouter les fetch dans la partie react

--- tp4/Spring/src/main/java/com/example/tp3/controller/ReacTodoController.java
+++
@@ -1,6 +0,0 @@
-package src.main.java.com.example.tp3.controller;
-
-@RestController
-public class ReacTodoController {
-}
-git
--- tp4/Spring/src/main/java/com/example/tp3/repository/DvdRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tp3.repository;
-
-import com.example.tp3.model.document.Document;
-import com.example.tp3.model.document.Dvd;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DvdRepository extends JpaRepository<Dvd, Long> {
-}
